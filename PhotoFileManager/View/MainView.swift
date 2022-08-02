1497e2a338e43452f1881d58dc711478a066a049
MichaelK <havinmichael@gmail.com>
2022-08-03 00:15:35 +0300

    feat!: Configure project for building UI without Storyboard

---
+++ PhotoFileManager/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  PhotoFileManager
+//
+//  Created by Michael Khavin on 03.08.2022.
+//
+
+import UIKit
+
+class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
